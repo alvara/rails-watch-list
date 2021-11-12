a9e8324dcbc8b6d30293379686cf47fe25c3c125
Jesse Alvarado <contact@jessealvarado.com>
2021-11-12 12:45:10 +0900

    setup home root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # lists #index, #show, #new, #create
-  resources :lists, only: [:index, :show, :new, :create] do
+  root to: 'lists#index'
+  resources :lists, only: [ :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
